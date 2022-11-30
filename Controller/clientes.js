63380cc6b27caa4e2ad2d221647579019e9ebdad
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-11-29 22:54:35 -0300

    Função validador de cpf

--- Controller/clientes.js
+++ Controller/clientes.js
@@ -14,6 +14,28 @@ if (fileExists(__dirname + '/db.json')) {
 }
 window.Vue = require('vue')
 
+// Validador
+function validadorCpf(cpf){
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+}
+
 
 let clientes = db.getCollection('Clientes')
 new Vue({
